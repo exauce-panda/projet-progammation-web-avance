94b6ad44b96199196202fb02d909558d15f46dd8
katabshack <79207842+katabshack@users.noreply.github.com>
2024-02-02 00:51:46 -0500

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Footer.module.css"
-
-export default function Footer() {
-    return <footer className={styles.footer}>
-        &copy; Jonathan Wilkie | Collège La Cité
-    </footer>
-}
