f99ae9f3e1974ef1796c615d66d13e32c8c8b141
katabshack <79207842+katabshack@users.noreply.github.com>
2024-02-08 17:20:35 -0500

    AlphaCenter final update

---
+++ app/layout.jsx
@@ -0,0 +1,120 @@
+'use client' 
+
+// Importations des composants et des styles nécessaires
+
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import 'normalize.css/normalize.css'
+import styles from "./layout.module.css"
+import Home from './Home'
+import Eve from '../components/eve'
+import Contact from '../components/Contact'
+import FAQ from '../components/Faq'
+import PageEve from '../components/pageEve'
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Styles Bootstrap pour les composants
+
+
+import imgEven1 from "@/public/MainEventImage/kcx.png"
+import imgEven2 from "@/public/MainEventImage/elevenallstar.jpeg"
+import imgEven3 from "@/public/MainEventImage/evenBox.png"
+import imgEven4 from "@/public/MainEventImage/superBall.jpg"
+
+import Head from 'next/head';
+
+// Importation des polices depuis Google Fonts via l'API next/font
+
+import { Inter, Roboto, Nunito, Poppins, Montserrat, Open_Sans } from "next/font/google";
+
+// Initialisation des polices avec configurations spécifiques
+
+const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
+const nunito = Nunito({ subsets: ["latin"], weight: "400" });
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
+const montserrat = Montserrat({ subsets: ["latin"], weight: "400" });
+const openSans = Open_Sans({ subsets: ["latin"], weight: "400" });
+
+
+export default function RootLayout({ children }) {
+
+  // Gestion de l'état pour la navigation entre les pages
+
+  const [page, setPage] = useState('Home')
+
+  let [pageId, setPageId] = useState('') 
+
+  // Données pour les événements, utilisées dans les composants enfants
+
+  const allEvents = [
+    {
+      id: 1,
+      image: imgEven1,
+      title: "KCX KCORP VS THE WORLD",
+      date: "Jeudi 8 Février 2024"
+    },
+    {
+      id: 2,
+      image: imgEven2,
+      title: "ELEVEN ALL STARS",
+      date: "Samedi 19 Novembre 2024"
+    },
+    {
+      id: 3,
+      image: imgEven3,
+      title: "CANADA VS POLAND BOX",
+      date: "Jeudi 8 Février 2024"
+    },
+    {
+      id: 4,
+      image: imgEven4,
+      title: "SUPERBOWL FINAL",
+      date: "Samedi 19 Novembre 2024"
+    }
+  ];
+
+
+  return (
+    <html lang="en">
+      <Head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Préchargement de la première image d'événement pour améliorer la performance de chargement */}
+
+        {allEvents.length > 0 && (
+          <link
+            rel="preload"
+            as="image"
+            href={allEvents[0].image.src}
+          />
+        )}
+      </Head>
+      <body className={`${montserrat.className} ${styles.principale} d-flex flex-column`}>
+        <Header setPage={setPage} />
+        <main className={`${styles.bob} container-fluide flex-grow-1 min-vh-100`}>
+          {
+            page === 'Home' ?
+            <Home setPage={setPage} allEvents={allEvents} setPageId={setPageId}/> 
+            :
+            page === 'Eve' ?
+            <Eve setPage={setPage} allEvents={allEvents} setPageId={setPageId}/>
+            :
+            page === 'Contact' ?
+            <Contact />
+            :
+            page === 'FAQ' ?
+            <FAQ />
+            :
+            page === 'pageEvent' ?
+            <PageEve event = {pageId} allEvents={allEvents} />
+            :
+            <div className={`container mx-auto py-5 my-5 text-center ${styles.notFound}`}>404 - Not Found </div> // Page 404 pour les chemins non trouvés
+          }
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
+
---
+++ components/Faq.jsx
@@ -0,0 +1,64 @@
+import styles from './Faq.module.css';
+import { useEffect } from 'react';
+
+// Définition d'un tableau d'objets contenant les données pour chaque FAQ
+
+const faqData = [
+  {
+    question: "Comment acheter des billets?",
+    answer: "Vous pouvez acheter des billets directement sur notre site web dans la page events vous pouvez choisir un evenement puis prendre un ticket.",
+    id: "one"
+  },
+  {
+    question: "Quelles sont les mesures sanitaires en place?",
+    answer: "Nous suivons les directives gouvernementales. Veuillez consulter la page de l'événement spécifique pour les mesures sanitaires les plus à jour.",
+    id: "two"
+  },
+  {
+    question: "Puis-je obtenir un remboursement si je ne peux pas assister?",
+    answer: "Les remboursements sont possibles sous certaines conditions. Veuillez consulter notre politique de remboursement ou contactez le service client pour plus d'informations.",
+    id: "three"
+  }
+];
+
+export default function Faq() {
+
+  // Utilisation de useEffect pour charger les scripts Bootstrap nécessaires pour le fonctionnement de l'accordéon
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      require('bootstrap/dist/js/bootstrap.bundle.min');
+    }
+  }, []);
+
+  return (
+    <div className={`container py-5 ${styles.pageFaq}`}>
+      <h1 className="text-center mb-4">Foire aux questions (FAQ)</h1>
+      <div className="accordion" id="accordionFAQ">
+
+        {/* Parcourt le tableau faqData pour créer un item d'accordéon pour chaque question */}
+        
+        {faqData.map(({ question, answer, id }) => (
+          <div className="accordion-item" key={id} >
+            <h2 className="accordion-header" id={`heading${id}`}>
+
+              {/* Bouton qui contrôle l'affichage de la réponse */}
+
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${id}`} aria-expanded="false" aria-controls={`collapse${id}`}>
+                {question}
+              </button>
+            </h2>
+
+            {/* Conteneur pour la réponse qui s'affiche ou se cache en fonction de l'état de l'accordéon */}
+
+            <div id={`collapse${id}`} className="accordion-collapse collapse" aria-labelledby={`heading${id}`} data-bs-parent="#accordionFAQ">
+              <div className="accordion-body">
+                {answer} {/* Affichage de la réponse */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
