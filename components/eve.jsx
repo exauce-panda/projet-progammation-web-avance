0225885e04f6ce6aab200de9889f0601a28261f0
jendel2024 <159080224+jendel2024@users.noreply.github.com>
2024-02-09 03:04:52 -0500

    Update eve.jsx

--- components/eve.jsx
+++ components/eve.jsx
@@ -2,11 +2,12 @@ import styles from './eve.module.css';
 import Events from './Events'
 
 export default function Eve({setPage, allEvents, setPageId}) {
-
   return (
     <>
+      {/* Conteneur principal des événements avec styles définis dans eve.module.css */}
       <div className={styles.eventsMain}>
-        <Events setPage = {setPage} allEvents = {allEvents} setPageId={setPageId}/>
+        {/* Composant Events pour afficher les événements */}
+        <Events setPage={setPage} allEvents={allEvents} setPageId={setPageId}/>
       </div>
     </>
   );
