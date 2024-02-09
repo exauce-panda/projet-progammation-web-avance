9ec8daedddf6eb9c5a50716384ad57d4fc67a68a
jendel2024 <159080224+jendel2024@users.noreply.github.com>
2024-02-09 02:47:30 -0500

    Mise à jour des commentaires dans Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,11 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css"; // Importation des styles CSS pour le Footer
 
+// Définition du composant Footer
 export default function Footer() {
     return (
         <footer className={`${styles.footer} text-center p-4 mt-1`}>
             <p>
-                {/* Affichage de l'année en cours grâce à JavaScript et mention des développeurs du projet AlphaCenter */}
-
+                {/* Affichage du copyright avec l'année en cours et les développeurs */}
                 &copy; {new Date().getFullYear()} Collège La Cité <br/>
                 Développé par Exauce Mpanda, Placide Kataba &amp; Jendel Alibi
             </p>
