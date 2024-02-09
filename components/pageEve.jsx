3da6878d352dd937b6b4fd8cb483e500a70be11f
jendel2024 <159080224+jendel2024@users.noreply.github.com>
2024-02-09 03:17:54 -0500

    Update pageEve.jsx

--- components/pageEve.jsx
+++ components/pageEve.jsx
@@ -1,12 +1,12 @@
-import styles from './pageEve.module.css';
-import PageEvent from './pageEvent'
+import styles from './pageEve.module.css'; // Importation des styles CSS spécifiques à ce composant
+import PageEvent from './pageEvent'; // Importation du composant PageEvent
 
-export default function PageEve({event}) {
+export default function PageEve({event}) { // Définition du composant PageEve qui prend un événement en paramètre
 
   return (
     <>
-      <div className={styles.pageEventMain}>
-        <PageEvent event = {event}  />
+      <div className={styles.pageEventMain}> {/* Division principale avec une classe de style spécifique */}
+        <PageEvent event = {event}  /> {/* Affichage du composant PageEvent avec l'événement passé en tant que prop */}
       </div>
     </>
   );
