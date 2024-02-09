2f6fcf86d02f1b1b0315bad85fe139f958cbb7fe
jendel2024 <159080224+jendel2024@users.noreply.github.com>
2024-02-09 03:15:23 -0500

    Update MenuNav.jsx

--- components/MenuNav.jsx
+++ components/MenuNav.jsx
@@ -1,4 +1,4 @@
-import styles from './MenuNav.module.css'
+import styles from './MenuNav.module.css';
 
 export default function MenuNav({ setPage }) {
 
@@ -9,19 +9,18 @@ export default function MenuNav({ setPage }) {
           {/* Chaque lien de navigation appelle setPage avec le nom de la page correspondante lors du clic */}
           
           <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => setPage('Home')}>Home</a>
+            <a className="nav-link" href="#" onClick={() => setPage('Home')}>Accueil</a> {/* Lien vers la page d'accueil */}
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => setPage('Eve')}>Events</a>
+            <a className="nav-link" href="#" onClick={() => setPage('Eve')}>Événements</a> {/* Lien vers la page des événements */}
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => setPage('Contact')}>Contact</a>
+            <a className="nav-link" href="#" onClick={() => setPage('Contact')}>Contact</a> {/* Lien vers la page de contact */}
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => setPage('FAQ')}>FAQ</a>
+            <a className="nav-link" href="#" onClick={() => setPage('FAQ')}>FAQ</a> {/* Lien vers la page de FAQ */}
           </li>
         </ul>
       </nav>
     );
   }
-  
