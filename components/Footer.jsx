41ff1f641aa51a1dfb174eb2bf7ddbf8ee8c3335
exauce-panda <149726209+exauce-panda@users.noreply.github.com>
2024-03-11 20:42:19 -0400

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,24 @@
 import styles from "./Footer.module.css"; // Importation des styles CSS pour le Footer
+import logoInstagram from "@/public/footerLogoImage/instagram.jpg"
+import logoYoutube from "@/public/footerLogoImage/logoYoutube.jpg"
+import logoX from "@/public/footerLogoImage/x.png"
 
 // Définition du composant Footer
 export default function Footer() {
     return (
         <footer className={`${styles.footer} text-center p-4 mt-1`}>
             <p>
+                Telephone : 613 742-2483
+            </p>
+            <img src={logoX} alt="Logo X" />
+            <img src={logoInstagram} alt="Logo Instagram" />
+            <img src={logoYoutube} alt="Logo YouTube" />
+            <p>
                 {/* Affichage du copyright avec l'année en cours et les développeurs */}
                 &copy; {new Date().getFullYear()} Collège La Cité <br/>
                 Développé par Exauce Mpanda, Placide Kataba &amp; Jendel Alibi
             </p>
+            
         </footer>
     );
 }
